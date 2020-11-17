1d4725b908bfdba074bac12b6d43978dd82f0014
Valentina Cegarra T <valentinacegarra1@gmail.com>
2020-11-17 23:02:48 +0100

    Change the comments. Digits file.

--- digits.cs
+++ digits.cs
@@ -2,7 +2,7 @@
  * (C) IES San Vicente 2020. 1ºDAM Semipresencial. Valentina Cegarra.
  * 
  * Program that ask the user to enter a number to calculate the amount of digits (1, 2, 3 o more) of the inserted number.
- * When the user enters the word "end" the program finish. * 
+ * When the user enters the word "end" the program prints the number of digits. * 
  */
 using System;
 
