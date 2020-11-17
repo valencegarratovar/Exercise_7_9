8a984f58c7100ec84e4903ad7b01bca4f5adc3a4
Valentina Cegarra T <valentinacegarra1@gmail.com>
2020-11-17 17:33:12 +0100

    Spacing digits file

--- digits.cs
+++ digits.cs
@@ -5,25 +5,27 @@ class digits
     static void Main()
     {
         //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-        int a = 0, b = 0, c = 0, d = 0;
-
-        Console.Write("Insert a number: ");
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
 
         // read the number as string to check the word "end"
+        Console.Write("Insert a number: ");
         string e = Console.ReadLine();
 
-        while(e!="end")
+        while (e != "end")
         {
             int input = Convert.ToInt32(e);
 
-            if(input != 0)
+            if (input != 0)
             {
-                if(input/10 == 0)
-                    a ++;  // it only has one digit
-                else if (input/100 == 0)
-                    b ++;  // the number has 2 digits
-                else if (input/1000 == 0)
-                    c ++;  // the number has 3 digits
+                if (input / 10 == 0)
+                    a++;  // it only has one digit
+                else if (input / 100 == 0)
+                    b++;  // the number has 2 digits
+                else if (input / 1000 == 0)
+                    c++;  // the number has 3 digits
                 else
                     d++;  // the number has more than 3 digits
             }
