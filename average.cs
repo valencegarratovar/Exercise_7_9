1d689ae14ab05912130da94b3ebaaf16bff4bfba
Valentina Cegarra T <valentinacegarra1@gmail.com>
2020-11-17 20:33:07 +0100

    Change of variable names. Average file.

--- average.cs
+++ average.cs
@@ -4,28 +4,28 @@ public class Average
 {
 	public static void Main()
 	{
-		double a = 0;
-		double b;
-		double c = 0;
-		double d;
-		string s;
+		double notesSum = 0;
+		double mark;
+		double count = 0;
+		double noteAverage;
+		string insertedWord;
 
 		Console.Write("Insert a mark: ");
-		s = Console.ReadLine();	// we read the mark as string to check the word "end"
+		insertedWord = Console.ReadLine();	// we read the mark as string to check the word "end"
 
-		while (s != "end")
+		while (insertedWord != "end")
 		{  // if the mark isn't an end then we pass it to double to work with it
-			b = Convert.ToDouble(s);
-			c++;   //counter of numbers
-			a += b;  // sum of all the notes
+			mark = Convert.ToDouble(insertedWord);
+			count++;   //counter of numbers
+			notesSum += mark;  // sum of all the notes
 			Console.Write("Insert a mark: ");
-			s = Console.ReadLine();
+			insertedWord = Console.ReadLine();
 		}
 
-		if (c != 0)
+		if (count != 0)
 		{
-			d = a / c;  //calculate the average
-			Console.WriteLine("The average is {0}.", d);
+			noteAverage = notesSum / count;  //calculate the average
+			Console.WriteLine("The average is {0}.", noteAverage);
 		}
 	}
 }
